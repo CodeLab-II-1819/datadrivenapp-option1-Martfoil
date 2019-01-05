21326d1e7b84e02378413d12cee7e39851cb2beb
Martfoil <martin.schiller17@bathspa.ac.uk>
2019-01-05 20:51:44 +0000

    Update Source.cpp
    Options added (without functions to load next area).

--- DataDriApp MS/Source.cpp
+++ DataDriApp MS/Source.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int main() {
 
-	cout << "Hello World" << endl;
+	cout << "Welcome to the Twitter Interrogation Program (TIP)!" << endl;
+	cout << "The TIP allows you to interrogate tweets from the top 20 users from our 'sampleTweets' database." << endl;
+	cout << "Below are queries that you are able to use as well as the option number it is equal to:" << endl;
+	cout << "Count the total number of tweets in the data set: key press '1'." << endl;
+	cout << "Count the number of tweets that mention the word money : key press '2'." << endl;
+	cout << "Count the number of tweets that mention politics : key press '3'." << endl;
+	cout << "Print to the screen any tweets mentioning the word 'Paris' : key press '4'." << endl;
+	cout << "Print to the screen any tweets mentioning the word 'DreamWorks' : key press '5'." << endl;
+	cout << "Print to the screen any tweets mentioning the word 'Uber' : key press '6'." << endl;
+	cout << "Plus a minimum of 4 queries of your own." << endl;
+	cout << "Please press the corresponding number key to decide what you would like to do." << endl;
+
+	int select;
+	cin >> select;
 
 	return 0;
 }
