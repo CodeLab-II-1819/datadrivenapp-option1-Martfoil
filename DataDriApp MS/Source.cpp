b1181c8b03ea408f6e401cbe236565ae226bd93c
Martfoil <martin.schiller17@bathspa.ac.uk>
2019-01-31 07:10:23 +0000

    Update Source.cpp
    Final Data Driven app with added content for final voids and comments.

--- DataDriApp MS/Source.cpp
+++ DataDriApp MS/Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include<fstream>
+#include<fstream> //allows file handling
 #include <string>
-#include <vector>
+//declarations for voids
 void menustart();
 void option1();
 void option2();
@@ -17,13 +17,13 @@ void option0();
 using namespace std;
 
 
-int main() {
+int main() { //section of code run first
 	cout << "Welcome to the Twitter Interrogation Program (TIP)!" << endl;
 	cout << "The TIP allows you to interrogate tweets from the top 20 users from our 'sampleTweets' database." << endl;
-	menustart();
+	menustart(); //loads menustart function
 }
 
-void menustart() {
+void menustart() { //user is prompted with text related to info about tip
 	cout << "Below are queries that you are able to use as well as the option number it is equal to:" << endl;
 	cout << "Count the total number of tweets in the data set: key press '1'." << endl;
 	cout << "Count the number of tweets that mention the word money : key press '2'." << endl;
@@ -31,11 +31,14 @@ void menustart() {
 	cout << "Print to the screen any tweets mentioning the word 'Paris' : key press '4'." << endl;
 	cout << "Print to the screen any tweets mentioning the word 'DreamWorks' : key press '5'." << endl;
 	cout << "Print to the screen any tweets mentioning the word 'Uber' : key press '6'." << endl;
-	cout << "Plus a minimum of 4 queries of your own." << endl;
+	cout << "Print to the screen any tweets mentioning the word 'Uni' : key press '7'." << endl;
+	cout << "Print to the screen any tweets mentioning the word 'Brexit' : key press '8'." << endl;
+	cout << "Count the number of tweets that mention football : key press '9'." << endl;
+	cout << "Count the number of tweets that mention food : key press '0'." << endl;
 	cout << "Please press the corresponding number key to decide what you would like to do." << endl;
 
-	int select;
-	cin >> select;
+	int select; //int variable made for selection
+	cin >> select; //user inputs select
 
 	while ((select != 1) && (select != 2) && (select != 3) && (select != 4) && (select != 5) && (select != 6) && (select != 7) && (select != 8) && (select != 9) && (select != 0)) {
 		//if the users input is not equal to the assigned options, the user is asked to try again.
@@ -45,8 +48,8 @@ void menustart() {
 		cout << "Invalid input. Try again." << endl; //user is prompted to try again
 		cin >> select; //the user then gets to input an option again
 	}
-	if (select == 1) {
-		option1();
+	if (select == 1) { // if the user inputs '1'
+		option1(); //user is taken to option1() function
 	}
 	if (select == 2) {
 	option2();
@@ -84,11 +87,11 @@ void option1() {
 	string tweets; // string for holding tweets in data set
 	ifstream infile;
 	infile.open("sampleTweets.csv");
-	while (getline(infile, tweets)) {
+	while (getline(infile, tweets)) { //retrieves tweets from file. Continues to loop until the end is reached
 		++total; // adds onto total for every tweet counted
 	}
 	cout << "Number of tweets = " << total << endl; //number of tweets is outputted
-	cout << "Would you like to search for something else?" << endl;
+	cout << "Would you like to search for something else?" << endl; //user prompted to select next stage
 	cout << "Press '1' for the main menu." << endl;
 	cout << "Press '2' to exit." << endl;
 	int choose;
@@ -103,7 +106,7 @@ void option1() {
 		menustart();
 	}
 	if (choose == 2) {
-		system("pause");
+		system("pause"); //user is shown paused code. when user presses button, the code exits
 	}
 	
 }
@@ -153,11 +156,11 @@ void option3() {
 	if (infile.good()) {
 		while (!infile.eof()) {
 			getline(infile, word);
-			if (word.find("Politics") != string::npos) {
-				++total;
+			if (word.find("Politics") != string::npos) { //if statement runs when politics is found
+				++total; //total is added up
 			}
 		}
-		cout << "Total of tweets mentioning Politics: " << total << endl;
+		cout << "Total of tweets mentioning Politics: " << total << endl; //added total is outputted here
 		cout << "Would you like to search for something else?" << endl;
 		cout << "Press '1' for the main menu." << endl;
 		cout << "Press '2' to exit." << endl;
@@ -284,15 +287,139 @@ void option6() {
 }
 void option7() {
 	cout << "Option 7 selected" << endl;
+	cout << "Now searching for tweets mentioning uni" << endl;
+	string word;
+	ifstream infile;
+	infile.open("sampleTweets.csv");
+
+	if (infile.good()) {
+		while (!infile.eof()) {
+			getline(infile, word);
+			if (word.find("Uni") != string::npos) {
+				cout << "Found Uni in tweet: " << word << endl;
+				cout << "_______________________________________________________________________________________________________________________________________" << endl;
+			}
+		}
+	}
+	cout << "Would you like to search for something else?" << endl;
+	cout << "Press '1' for the main menu." << endl;
+	cout << "Press '2' to exit." << endl;
+	int choose;
+	cin >> choose;
+	while ((choose != 1) && (choose != 2)) {
+		cin.clear();
+		cin.ignore();
+		cout << "invalid input please try again" << endl;
+		cin >> choose;
+	}
+	if (choose == 1) {
+		menustart();
+	}
+	if (choose == 2) {
+		system("pause");
+	}
 }
 void option8() {
 	cout << "Option 8 selected" << endl;
+	cout << "Now seaching for tweets mentioning Brexit" << endl;
+	string word;
+	ifstream infile;
+	infile.open("sampleTweets.csv");
+
+	if (infile.good()) {
+		while (!infile.eof()) {
+			getline(infile, word);
+			if (word.find("Brexit") != string::npos) {
+				cout << "Found Brexit in tweet: " << word << endl;
+				cout << "_______________________________________________________________________________________________________________________________________" << endl;
+			}
+		}
+	}
+	cout << "Would you like to search for something else?" << endl;
+	cout << "Press '1' for the main menu." << endl;
+	cout << "Press '2' to exit." << endl;
+	int choose;
+	cin >> choose;
+	while ((choose != 1) && (choose != 2)) {
+		cin.clear();
+		cin.ignore();
+		cout << "invalid input please try again" << endl;
+		cin >> choose;
+	}
+	if (choose == 1) {
+		menustart();
+	}
+	if (choose == 2) {
+		system("pause");
+	}
 }
 void option9() {
 	cout << "Option 9 selected" << endl;
+	cout << "Now counting tweets mentioning football" << endl;
+	int total = 0;
+	string word;
+	ifstream infile;
+	infile.open("sampleTweets.csv");
+	if (infile.good()) {
+		while (!infile.eof()) {
+			getline(infile, word);
+			if (word.find("Football") != string::npos) {
+				++total;
+			}
+		}
+		cout << "Total of tweets mentioning Football: " << total << endl;
+		cout << "Would you like to search for something else?" << endl;
+		cout << "Press '1' for the main menu." << endl;
+		cout << "Press '2' to exit." << endl;
+		int choose;
+		cin >> choose;
+		while ((choose != 1) && (choose != 2)) {
+			cin.clear();
+			cin.ignore();
+			cout << "invalid input please try again" << endl;
+			cin >> choose;
+		}
+		if (choose == 1) {
+			menustart();
+		}
+		if (choose == 2) {
+			system("pause");
+		}
+	}
 }
 void option0() {
 	cout << "Option 0 selected" << endl;
+	cout << "Now counting tweets mentioning food" << endl;
+	int total = 0;
+	string word;
+	ifstream infile;
+	infile.open("sampleTweets.csv");
+	if (infile.good()) {
+		while (!infile.eof()) {
+			getline(infile, word);
+			if (word.find("Food") != string::npos) {
+				++total;
+			}
+		}
+		cout << "Total of tweets mentioning Food: " << total << endl;
+		cout << "Would you like to search for something else?" << endl;
+		cout << "Press '1' for the main menu." << endl;
+		cout << "Press '2' to exit." << endl;
+		int choose;
+		cin >> choose;
+		while ((choose != 1) && (choose != 2)) {
+			cin.clear();
+			cin.ignore();
+			cout << "invalid input please try again" << endl;
+			cin >> choose;
+		}
+		if (choose == 1) {
+			menustart();
+		}
+		if (choose == 2) {
+			system("pause");
+		}
+	}
 }
 
 
